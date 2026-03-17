a2bff17aacc3b36ede4921cc67d890f14e7813f8
memoryleak47 <memoryleak47@protonmail.ch>
2026-03-17 17:40:38 +0100

    start normal e-graph stuff

--- main.py
+++ main.py
@@ -6,6 +6,34 @@ class Class:
         pass
 
 class EGraph:
-    def __init__(self, pgraph: PGraph):
+    def __init__(self, rules: list[Rule]):
+        self.pgraph = PGraph(rules)
+
         self.classes = {} # Id -> Class
-        self.pgraph = pgraph
+        self.hashcons = {} # Node -> Id
+        self.uf = {} # Id -> Id
+
+    def add_node(self, n: Node):
+        n = Node(n.f, tuple(map(self.find, n.args)))
+        if n not in self.hashcons:
+            i = Id(len(self.classes))
+            self.classes[i] = Class()
+            self.uf[i] = i
+            self.hashcons[n] = i
+        return self.hashcons[n]
+
+    def find(self, x: Id) -> Id:
+        while True:
+            y = self.uf[x]
+            if x == y: return x
+            x = y
+
+    def union(self, x: Id, y: Id):
+        x = self.find(x)
+        y = self.find(y)
+        if x == y: return
+        self.uf[x] = y
+        self.rebuild()
+
+    def rebuild(self):
+        ...
