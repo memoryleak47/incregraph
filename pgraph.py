e3bdba9d18325d43caabc6d1abd4529a49518ea3
memoryleak47 <memoryleak47@protonmail.ch>
2026-03-17 16:59:23 +0100

    start with pgraph.py

--- pgraph.py
+++ pgraph.py
@@ -1,11 +1,74 @@
 from dataclasses import dataclass
 from types import *
 
+# This effectively is a slotted hashcons.
+
+type Shape = PNode # a pnode where all variables are renamed in order 0..n
+
 class PClass:
-    def __init__(self, node: PNode):
-        self.arity = arity
-        self.node = node
+    def __init__(self, sh: Shape):
+        self.sh = sh
+        self.reactors = [] # : list[PTerm], applicable rules from here
 
 class PGraph:
-    def __init__(self):
-        self.classes = {} # PId -> PClass
+    def __init__(self, rules: list[Rule]):
+        self.pclasses = {} # PId -> PClass
+        self.hashcons = {} # PNode -> PId
+
+        for r in rules:
+            self.add_rule(r)
+
+    def add_pnode(self, pnode: PNode) -> AppliedPId:
+        (sh, args) = shape(pnode)
+        if sh not in self.hashcons:
+            pid = PId(len(self.pclasses))
+            self.pclasses[pid] = Class(sh)
+            self.hashcons[sh] = pid
+        return AppliedPId(self.hashcons[sh], args)
+        
+    def add_term(self, t: PTerm) -> AppliedPId:
+        pass
+
+    def add_rule(self, rule: Rule):
+        app_pid = self.add_term(self.lhs)
+        renamed = rename(rule.rhs, app_pid.args)
+        self.classes[app_pid.pid].reactors.append(renamed)
+
+def rename(x, f: Fn[PVar, PVar]):
+    if isinstance(x, PVar):
+        return f(x)
+    if isinstance(x, AppliedPId):
+        args = tuple(map(lambda x: rename(x, f), x.args))
+        return AppliedPId(x.pid, args)
+    if isinstance(x, App):
+        args = tuple(map(lambda x: rename(x, f), x.args))
+        return App(x.f, args)
+    assert(False)
+
+def shape(pnode: PNode) -> (Shape, tuple[PVar]):
+    d = dict() # PVar "input" -> PVar "shape"
+    def zip_d(t):
+        out = []
+        for v in t:
+            assert(isinstance(v, PVar))
+            if v not in d: d[v] = PVar(len(d))
+            out.append(d[v])
+        return tuple(out)
+
+    new_args = []
+    for a in pnode.args:
+        if isinstance(a, PVar):
+            new_args.append(zip_d((a,))[0])
+        else:
+            assert(isinstance(a, AppliedPId))
+            new_args.append(
+                AppliedPId(a.pid, zip_d(a.args))
+            )
+    shape = PNode(pnode.f, new_args)
+
+    args = []
+    for (v, pos) in d.items():
+        args[pos] = v
+    args = tuple(args)
+    
+    return shape, args
--- types.py
+++ types.py
@@ -1,17 +1,33 @@
 from dataclasses import dataclass
 
-type Id = int # corresponds to e-graph e-classes
-type PId = int # corresponds to patterns
-
 @dataclass(frozen=True)
 class App: # generic over T
     f: str
     args: tuple[T]
 
+@dataclass(frozen=True)
+class Id: # corresponds to e-graph e-classes
+    id_i: int
+
 type Node = App # which recurses via Id
 type Term = App # which recurses via Term
 
-type PNode = App # which recurses via PId
+# -- pattern things --
+
+@dataclass(frozen=True)
+class PId # corresponds to patterns
+    pid_i: int
+
+@dataclass(frozen=True)
+class AppliedPId:
+    pid: PId
+    args: tuple[PVar]
+
+@dataclass(frozen=True)
+class PVar: # aka 'Slot'
+    var_i: int
+
+type PNode = App # which recurses via AppliedPId|PVar
 type PTerm = App # which recurses via PTerm (PTerm = pattern)
 
 @dataclass(frozen=True)
