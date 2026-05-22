b43c6dec80be4e415776ed21094cbdccfb8497d1
memoryleak47 <memoryleak47@protonmail.ch>
2026-05-22 15:08:54 +0200

    new test

--- rs/src/tests.rs
+++ rs/src/tests.rs
@@ -1,6 +1,7 @@
 use crate::*;
 
 fn plus(l: Pattern, r: Pattern) -> Pattern { Pattern::Node(Symbol::new("plus"), Box::new([l, r])) }
+fn mul(l: Pattern, r: Pattern) -> Pattern { Pattern::Node(Symbol::new("mul"), Box::new([l, r])) }
 fn neg(l: Pattern) -> Pattern { Pattern::Node(Symbol::new("neg"), Box::new([l])) }
 fn zero() -> Pattern { Pattern::Node(Symbol::new("zero"), Box::new([])) }
 fn one() -> Pattern { Pattern::Node(Symbol::new("one"), Box::new([])) }
@@ -38,6 +39,48 @@ fn test1() {
     let r = eg.add_term(zero());
 
     for _ in 0..10 {
+        eg.tick();
+    }
+    assert_eq!(eg.find(l), eg.find(r));
+}
+
+
+#[test]
+fn test2() {
+    let mut pgraph = PGraph::new();
+
+    pgraph.add_rule(
+        mul(x(), zero()),
+        zero()
+    );
+
+    pgraph.add_rule(
+        mul(x(), one()),
+        x()
+    );
+
+    pgraph.add_rule(
+        mul(x(), y()),
+        mul(y(), x())
+    );
+
+    pgraph.add_rule(
+        plus(x(), zero()),
+        x()
+    );
+
+    pgraph.add_rule(
+        plus(x(), y()),
+        plus(y(), x()),
+    );
+
+    let mut eg = EGraph::new(&pgraph);
+
+    // Proving that: (a * 0) + (1 * a) == a
+    let l = eg.add_term(plus(mul(a(), zero()), mul(one(), a())));
+    let r = eg.add_term(a());
+
+    for _ in 0..20 {
         eg.tick();
     }
     assert_eq!(eg.find(l), eg.find(r));
